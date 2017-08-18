48dfe3447ed6f073d77c86e33b69e0e64229022c
volosovich <andreyvolosovich@gmail.com>
2017-08-19 00:04:11 +0300

    added fix for docs

--- test/js/first.js
+++ test/js/first.js
@@ -17,3 +17,15 @@ anotherName(params);
 
 let a = new Class();
 const b = Class();
+
+
+/**
+   * Required to validate if the target container is correct.
+   *
+   * @param container {Object|String} The container to validate.
+   * @return {boolean} If the container is valid or not.
+   * @private
+   */
+var test_a = test1.bind(this, arg1, arg2);
+var testb = test2.call(this, arg1, arg2);
+var testc = test3.apply(this, arg1, arg2);
