84c257733fcced8c5dbc87600f23b6b20a082b38
Eddy Hintze <eddy@hintze.co>
2019-06-06 07:40:33 -0400

    Added python decorators as best as I can

--- test/python/test.py
+++ test/python/test.py
@@ -1,6 +1,7 @@
 from .foo import bar as b
 from ..foo1 import bar as b1
 
+
 class Foo(object):
 
     def __init__(self):
@@ -8,14 +9,18 @@ class Foo(object):
         """
         super().__init__()
 
-    def alpha(self):
+    @staticmethod
+    def alpha():
         for _, i in enumerate(['a', 'b'], start=1):
-          print(f"#{i}")
+            print(f"#{i}")
 
 
+@test_a()
+@foo.bar('abc')
 def test_a(b='test'):
     a = [1, 2, 3]
     return None
+
 
 # test
 test_a()
